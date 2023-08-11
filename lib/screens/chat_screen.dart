1e820418a98166200ccec6719ec3c7c147714987
YOGESH JAISANKAR <yogeshjaisnkar@gmail.com>
2023-08-12 02:08:14 +0530

    chat screen keyboard fixed

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:flutter/material.dart';
 
 class ChatScreen extends StatefulWidget {
@@ -13,98 +16,166 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   bool _isTyping = false;
+  FocusNode _textFieldFocusNode = FocusNode();
+  bool _showEmoji = false;
+  double _emojiPickerHeight = 0.0;
+
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _textFieldFocusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 3,
-        title: Row(
-          children: [
-            CircleAvatar(
-              radius: 20,
-              backgroundImage: CachedNetworkImageProvider(widget.profileImage),
-            ),
-            const SizedBox(width: 10),
-            Text(widget.userName),
-          ],
-        ),
-      ),
-      body: Column(
-        children: [
-          const Expanded(
-            child: Center(
-              child: Text("Chat messages interface here"), // Replace with actual chat messages UI
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20), // Adjust the radius as needed
-                border: Border.all(
-                  color: Colors.grey, // You can customize the border color here
-                ),
+    return GestureDetector(
+      onTap: () {
+        _textFieldFocusNode.unfocus();
+        _closeEmojiPicker();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 3,
+          title: Row(
+            children: [
+              CircleAvatar(
+                radius: 20,
+                backgroundImage:
+                CachedNetworkImageProvider(widget.profileImage),
               ),
-              child: Row(
-                children: [
-                  IconButton(
-                    icon: const Icon(Icons.mood),
-                    onPressed: () {
-                      // Add mood functionality here
-                      print("Mood button pressed");
-                    },
+              const SizedBox(width: 10),
+              Text(widget.userName),
+            ],
+          ),
+        ),
+        resizeToAvoidBottomInset: true,
+        body: Column(
+          children: [
+            Expanded(
+              child: Center(
+                child: Text("Chat messages interface here"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), // Adjust the radius as needed
+                  border: Border.all(
+                    color: Colors.grey, // You can customize the border color here
                   ),
-                  if (!_isTyping) ...[
-                    IconButton(
-                      icon: const Icon(Icons.attach_file),
-                      onPressed: () {
-                        // Add file attachment functionality here
-                        print("File attachment button pressed");
-                      },
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5.0),
+                          child: IconButton(
+                            icon: const Icon(Icons.mood),
+                            onPressed: () {
+                              _toggleEmojiPicker();
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: SingleChildScrollView(
+                            child: TextField(
+                              cursorColor: Colors.teal,
+                              focusNode: _textFieldFocusNode,
+                              controller: _textController,
+                              decoration: InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Type your message...',
+
+                              ),
+                              maxLines: 5,
+                              minLines: 1,
+                              textInputAction: TextInputAction.newline,
+                              onChanged: (text) {
+                                setState(() {
+                                  _isTyping = text.isNotEmpty;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 2),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 2, right: 4, top: 2, bottom: 2),
+                          child: CircleAvatar(
+                            backgroundColor: Colors.black12,
+                            child: IconButton(
+                              icon: const Icon(
+                                Icons.send,
+                                color: Colors.black54,
+                              ),
+                              onPressed: () {
+                                _closeEmojiPicker();
+                                _textFieldFocusNode.unfocus();
+                                print("Sent to ${widget.userName}");
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ],
-                  Expanded(
-                    child: TextField(
-                      decoration: InputDecoration(
-                        border:InputBorder.none,
-                        hintText: 'Type your message...',
-                        contentPadding: EdgeInsets.symmetric(horizontal: 10),
-                      ),
-                      maxLines: null,
-                      textInputAction: TextInputAction.newline,
-
-                      onChanged: (text) {
-                        setState(() {
-                          _isTyping = text.isNotEmpty;
-                        });
-                      },
-                    ),
-                  ),
-                  const SizedBox(width: 2),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 2,right: 4,top: 2,bottom: 2),
-                    child: CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.black12,
-                      child: IconButton(
-                        icon: const Icon(
-                          Icons.send,
-                          color: Colors.black54,
-                        ),
-                        onPressed: () {
-                          // Add send functionality here
-                          print("Sent to ${widget.userName}");
-                        },
-                      ),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
+          ],
+        ),
+        bottomNavigationBar: _showEmoji
+            ? SizedBox(
+          height: _emojiPickerHeight,
+          child: EmojiPicker(
+            textEditingController: _textController,
+            config: Config(
+              indicatorColor: Colors.teal,
+              iconColorSelected: Colors.teal,
+              columns: 7,
+              initCategory: Category.SMILEYS,
+              emojiSizeMax: 20 * (Platform.isAndroid ? 1.30 : 1.0),
+              verticalSpacing: 0,
+              horizontalSpacing: 0,
+            ),
+            onEmojiSelected: (emoji, category) {
+              print("Selected emoji: $emoji");
+            },
           ),
-        ],
+        )
+            : null,
       ),
     );
   }
+
+  void _toggleEmojiPicker() {
+    setState(() {
+      _showEmoji = !_showEmoji;
+      if (_showEmoji) {
+        _textFieldFocusNode.unfocus();
+      }
+    });
+
+    // Calculate the height of the EmojiPicker based on keyboard height
+    if (_showEmoji) {
+      final keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
+      _emojiPickerHeight = keyboardHeight;
+    } else {
+      _emojiPickerHeight = 0.0;
+    }
+  }
+
+  void _closeEmojiPicker() {
+    if (_showEmoji) {
+      setState(() {
+        _showEmoji = false;
+      });
+    }
+  }
 }
+
